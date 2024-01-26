14164b2cb1c1a45c49354a9541b096c91081dee1
Nitish Kumar <91400264+Nitishperfectionist@users.noreply.github.com>
2024-01-26 18:25:48 +0530

    Add files via upload

---
+++ MacBookPro14.js
@@ -0,0 +1,69 @@
+import "./MacBookPro14.css";
+
+const MacBookPro14 = () => {
+  return (
+    <div className="macbook-pro-14-1">
+      <div className="rectangle-parent">
+        <div className="group-child" />
+        <img className="icon" alt="" src="/2-1-1@2x.png" />
+        <div className="group-parent">
+          <div className="group-container">
+            <div className="rectangle-group">
+              <div className="group-item" />
+              <div className="get-started">Get Started</div>
+            </div>
+            <div className="check-your-financial">
+              Check your financial health
+            </div>
+            <div className="use-weathometer-to-container">
+              <p className="use-weathometer-to">
+                Use WeathoMeter to get a free report
+              </p>
+              <p className="use-weathometer-to">{`card for your finances- within minutes! `}</p>
+            </div>
+            <img className="icon1" alt="" src="/4-1-1@2x.png" />
+          </div>
+          <div className="expected-retirement-age-parent">
+            <div className="expected-retirement-age">
+              Expected Retirement Age
+            </div>
+            <div className="identify-mistakes">Identify Mistakes</div>
+            <div className="group-inner" />
+            <div className="line-div" />
+            <img className="icon2" alt="" src="/1-1-1@2x.png" />
+            <img className="icon3" alt="" src="/1-1-1@2x.png" />
+          </div>
+          <div className="personalised-road-map-parent">
+            <div className="expected-retirement-age">Personalised Road Map</div>
+            <div className="identify-mistakes">Tips To Improve</div>
+            <div className="group-inner" />
+            <div className="line-div" />
+            <img className="icon2" alt="" src="/1-1-1@2x.png" />
+            <img className="icon3" alt="" src="/1-1-1@2x.png" />
+          </div>
+        </div>
+        <img className="vector-icon" alt="" src="/vector-1.svg" />
+        <div className="rectangle-container">
+          <div className="group-item" />
+          <div className="get-started">Get Started</div>
+        </div>
+        <b className="how-it-works">How it works?</b>
+        <img className="icon6" alt="" src="/1-1@2x.png" />
+        <div className="answer-few-questions-container">
+          <p className="use-weathometer-to">Answer few</p>
+          <p className="use-weathometer-to">questions</p>
+        </div>
+        <div className="register-using-phone-container">
+          <p className="use-weathometer-to">Register using</p>
+          <p className="use-weathometer-to">phone and OTP</p>
+        </div>
+        <div className="get-report-and-container">
+          <p className="use-weathometer-to">Get report and</p>
+          <p className="use-weathometer-to">personal roadmap</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MacBookPro14;
